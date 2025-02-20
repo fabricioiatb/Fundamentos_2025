04ebef29df7ab6c5c5e53e1b1b75a7c1e6c26a39
Fabricio Freitas <159460536+fabricioiatb@users.noreply.github.com>
2025-02-19 21:50:23 -0300

    Update aula2.py

--- exercícios/aula2.py
+++ exercícios/aula2.py
@@ -15,7 +15,6 @@ dias = int(input("Digite a quantidade de dias: "))
 horas = int(input("Digite a quantidade de horas: "))
 minutos = int(input("Digite a quantidade de minutos: "))
 segundos = int(input("Digite a quantidade de segundos: "))
-total_segundos = (dias * 24 * 60 * 60) + (horas * 60 * 60) + (minutos * 60) + segundos
+total = (dias * 24 * 60 * 60) + (horas * 60 * 60) + (minutos * 60) + segundos
+print(f"O total em segundos é: {total}")
 
-
-print(f"O total em segundos é: {total_segundos}")
