e96ad3d41175398039aa4d78892f346d5c75fb84
Fabricio Freitas <159460536+fabricioiatb@users.noreply.github.com>
2025-02-19 21:56:42 -0300

    Update aula2.py

--- exercícios/aula2.py
+++ exercícios/aula2.py
@@ -19,16 +19,16 @@ total = (dias * 24 * 60 * 60) + (horas * 60 * 60) + (minutos * 60) + segundos
 print(f"O total em segundos é: {total}")
 
 #ex4
-salario_por_hora = float(input("Quanto você ganha por hora? R$ "))
-horas_trabalhadas = float(input("Quantas horas você trabalha no mês? "))
-salario_bruto = salario_por_hora * horas_trabalhadas
-desconto_imposto_renda = salario_bruto * 0.11
+salarioh = float(input("Quanto você ganha por hora? R$ "))
+horastb = float(input("Quantas horas você trabalha no mês? "))
+salario_bruto = salarioh * horastb
+descontoir = salario_bruto * 0.11
 desconto_inss = salario_bruto * 0.08
 desconto_sindicato = salario_bruto * 0.05
-salario_liquido = salario_bruto - desconto_imposto_renda - desconto_inss - desconto_sindicato
+salario_liquido = salario_bruto - descontoir - desconto_inss - desconto_sindicato
 
 print(f"\nSalário bruto: R$ {salario_bruto:.2f}")
-print(f"Desconto imposto de renda (11%): R$ {desconto_imposto_renda:.2f}")
+print(f"Desconto imposto de renda (11%): R$ {descontoir:.2f}")
 print(f"Desconto INSS (8%): R$ {desconto_inss:.2f}")
 print(f"Desconto sindicato (5%): R$ {desconto_sindicato:.2f}")
 print(f"Salário líquido: R$ {salario_liquido:.2f}")
